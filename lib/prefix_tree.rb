5eab6bae9201eecd95a4c4f342ee848994f53ee3
Eric Roos <eric.roos@zendesk.com>
2022-05-06 10:54:21 -0500

    fix names etc

---
+++ lib/prefix_tree.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "prefix_tree/version"
+
+module PrefixTree
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/prefix_tree_rb_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+RSpec.describe PrefixTree do
+  it "has a version number" do
+    expect(PrefixTree::VERSION).not_to be nil
+  end
+
+end
